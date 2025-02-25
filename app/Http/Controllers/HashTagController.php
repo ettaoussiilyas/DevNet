c446e7f68792459aecfeb54f5a9a322e4e17ceed
ettaoussi <ettaoussiilyas0.1@gmail.com>
2025-02-25 12:14:51 +0000

    DEV-2 Data Base Migration Creation Files

---
+++ app/Http/Controllers/HashTagController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\HashTag;
+use App\Http\Requests\StoreHashTagRequest;
+use App\Http\Requests\UpdateHashTagRequest;
+
+class HashTagController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreHashTagRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(HashTag $hashTag)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(HashTag $hashTag)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateHashTagRequest $request, HashTag $hashTag)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(HashTag $hashTag)
+    {
+        //
+    }
+}
